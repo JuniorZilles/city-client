4dff37f7716cbebd4a6e927acc66a5fe1d26b570
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 16:43:00 -0300

    feat: include main required functions

---
+++ src/app/repository/Cities.js
@@ -0,0 +1,13 @@
+const Cities = require('../models/Cities')
+
+const create = (payload) => {}
+
+const getByName = (cityName)=>{}
+
+const getByState = (state)=>{}
+
+module.exports= {
+    create,
+    getByName,
+    getByState
+}
