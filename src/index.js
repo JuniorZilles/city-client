9134cb9026795744c64f4cf90555b80875732ffa
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 09:22:52 -0300

    feat: inclusão da estrutura básica do projeto, ps. não tinha salvado o arquivo

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const app = express()
 
 app.use(express.json())
 
-//app.use(require('./routes'));
+app.use(require('./routes'));
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("API Running")
