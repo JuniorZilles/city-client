ee51cc96d7ffb0992d08769fd8bb79f738ded0de
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 16:02:02 -0300

    fix: add full table definition

--- src/app/models/Cities.js
+++ src/app/models/Cities.js
@@ -1,11 +1,37 @@
 module.exports = (sequelize, DataTypes) => {
-    const Cities = sequelize.define('cities',{
-        id: DataTypes.INTEGER,
-        name: DataTypes.STRING,
-        state: DataTypes.STRING,
-        createdAt: DataTypes.DATE,
-        updatedAt: DataTypes.DATE,
-        version: DataTypes.INTEGER
-    })
-    return Cities
+  const Cities = sequelize.define(
+    'cities',
+    {
+      id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true,
+        allowNull: false
+      },
+      name: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      state: {
+        type: DataTypes.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: DataTypes.DATE,
+        allowNull: false
+      },
+      version: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+      }
+    },
+    {
+      timestamps: true
+    }
+  )
+  return Cities
 }
