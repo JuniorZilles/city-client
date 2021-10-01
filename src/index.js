f4894454262a36b2581e3bad1179aef29364db7b
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 09:22:08 -0300

    feat: inclusão da estrutura básica do projeto

---
+++ src/index.js
@@ -0,0 +1,15 @@
+require('dotenv').config({
+    path:process.env.NODE_ENV == 'test' ? '.env.test': '.env'
+})
+
+const express = require("express");
+
+const app = express()
+
+app.use(express.json())
+
+//app.use(require('./routes'));
+
+app.listen(process.env.PORT || 3000, ()=>{
+    console.log("API Running")
+})
