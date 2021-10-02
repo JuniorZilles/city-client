faacdcda102490486eee12bc485533d650d67460
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-02 10:58:30 -0300

    feat: include main routes

--- src/routes.js
+++ src/routes.js
@@ -1,1 +1,9 @@
-const routes = require('express').Router();
+const router = require('express').Router();
+
+const cities = require("./app/controllers/CitiesController")
+router.use("/api/v1/cities",cities)
+
+const clients = require("./app/controllers/ClientsController")
+router.use("/api/v1/clients",clients)
+
+module.exports = router
