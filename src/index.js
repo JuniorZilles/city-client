6bf5516c8dbebc8f006260af2dba8d9aaff81357
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-02 10:57:53 -0300

    fix: separete server from app to for testing

--- src/index.js
+++ src/index.js
@@ -2,13 +2,7 @@ require('dotenv').config({
     path:process.env.NODE_ENV == 'test' ? '.env.test': '.env'
 })
 
-const express = require("express")
-
-const app = express()
-
-app.use(express.json())
-
-//app.use(require('./routes'))
+const app = require('./app')
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("API Running")
