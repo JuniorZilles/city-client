25af3722169d338555e4fc420623211d2b66bf2a
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-02 19:27:33 -0300

    feat: include cities script for running the api

---
+++ src/app/controllers/CitiesController.js
@@ -0,0 +1,35 @@
+const router = require('express').Router()
+const MissingArgument = require('../errors/MissingArgument')
+const CitiesRepository = require('../repository/Cities')
+
+router.post('/', async (req, res, next) => {
+  try {
+    const body = req.body
+    const city = await CitiesRepository.create(body)
+
+    res.status(201).json(city)
+  } catch (error) {
+    next(error)
+  }
+})
+
+router.get('/', async (req, res, next) => {
+  try {
+    const name = req.query.name
+    const state = req.query.state
+    if (name === undefined && state === undefined) {
+      throw new MissingArgument('name or state')
+    }
+    let city
+    if (name != undefined) {
+      city = await CitiesRepository.getCityByName(name)
+    } else if (state != undefined) {
+      city = await CitiesRepository.getByState(state)
+    }
+    res.status(200).json(city)
+  } catch (error) {
+    next(error)
+  }
+})
+
+module.exports = router
