d34fc3be610dee222e861e8bf040b584b4f53705
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 23:06:03 -0300

    fix: remove ; from two lines

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ require('dotenv').config({
     path:process.env.NODE_ENV == 'test' ? '.env.test': '.env'
 })
 
-const express = require("express");
+const express = require("express")
 
 const app = express()
 
 app.use(express.json())
 
-//app.use(require('./routes'));
+//app.use(require('./routes'))
 
 app.listen(process.env.PORT || 3000, ()=>{
     console.log("API Running")
