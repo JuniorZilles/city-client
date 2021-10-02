120618e2718615c185b853ce055c10ecaf2337a4
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 21:30:06 -0300

    feat: include declaration of functions required for the client

---
+++ src/app/repository/Clients.js
@@ -0,0 +1,19 @@
+const {Clients} = require('../models')
+
+const createClient = payload =>{}
+
+const getClientByName = name =>{}
+
+const getClientById = id =>{}
+
+const removeClient = id => {}
+
+const updateClientName = (id, name) => {}
+
+module.exports = {
+    createClient,
+    getClientByName,
+    getClientById,
+    removeClient,
+    updateClientName
+}
