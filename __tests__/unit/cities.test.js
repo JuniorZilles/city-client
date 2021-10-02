175d9fb80ec41eea67a0ced80f8f8ad2123794b6
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 21:16:29 -0300

    feat: include the default tests for using cities repository

--- __tests__/unit/cities.test.js
+++ __tests__/unit/cities.test.js
@@ -1,5 +1,6 @@
 const truncate = require('../utils/truncate')
 const InvalidField = require('../../src/app/errors/InvalidField')
+const NotFound = require('../../src/app/errors/NotFound')
 const CitiesRepository = require('../../src/app/repository/Cities')
 const { expect } = require('@jest/globals')
 
@@ -27,20 +28,47 @@ describe('src :: app :: repository :: cities', () => {
 
   it('should throw a invalidfield exception to validate missing data', async () => {
     try {
-        const city = await CitiesRepository.create({})  
+      const city = await CitiesRepository.create({})
     } catch (error) {
-        expect(error).toBeInstanceOf(InvalidField)
+      expect(error).toBeInstanceOf(InvalidField)
     }
   })
 
   it('should get a city by name', async () => {
     const city_create = await CitiesRepository.create({
-        name: 'Dois Irmãos',
-        state: 'RS'
-      })
-    const city = await CitiesRepository.getByName("Dois Irmãos")
-    expect(city).toEqual(city_create)
+      name: 'Dois Irmãos',
+      state: 'RS'
+    })
+    const city = await CitiesRepository.getCityByName('Dois Irmãos')
+    expect(city.id).toEqual(city_create.id)
+    expect(city.name).toEqual(city_create.name)
+    expect(city.state).toEqual(city_create.state)
   })
 
-  it('should get a city by state', () => {})
+  it('should get throw a exception when city name is not found', async () => {
+    try {
+      const city = await CitiesRepository.getCityByName('Dois Irmãos')
+    } catch (error) {
+      expect(error).toBeInstanceOf(NotFound)
+    }
+  })
+
+  it('should get a city by state', async () => {
+    const city = ['Dois Irmãos', 'Rio Grande']
+    // promisse.all garante que todas as iterações feitas vão ser concluidas
+    await Promise.all(city.map(async name => {
+        await CitiesRepository.create({
+        name: name,
+        state: 'RS'
+      })
+    }))
+    const cities = await CitiesRepository.getByState('RS')
+    expect(cities.length).toEqual(2)
+  })
+
+  it('should return a empty list when doesnt find any city', async () => {
+    const cities = await CitiesRepository.getByState('RS')
+
+    expect(cities).toEqual([])
+  })
 })
