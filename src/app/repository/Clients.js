70d72b12c1b90ce7580073151c85b4fad2443ad5
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 23:04:20 -0300

    feat: include create client function

--- src/app/repository/Clients.js
+++ src/app/repository/Clients.js
@@ -1,19 +1,43 @@
-const {Clients} = require('../models')
+const moment = require('moment')
+const { Clients } = require('../models')
+const InvalidField = require('../errors/InvalidField')
 
-const createClient = payload =>{}
+const createClient = async payload => {
+  if (typeof payload.fullname !== 'string' || payload.fullname.length === 0) {
+    throw new InvalidField('fullname')
+  }
+  if (typeof payload.gender !== 'string' || payload.gender.length === 0) {
+    throw new InvalidField('gender')
+  }
+  if (typeof payload.birthday !== 'string' || payload.birthday.length === 0) {
+    throw new InvalidField('birthday')
+  }
+  if (typeof payload.city !== 'number') {
+    throw new InvalidField('city')
+  }
 
-const getClientByName = name =>{}
+  const birthday = moment(payload.birthday, 'DD/MM/YYYY').format(
+    'YYYY-MM-DD HH:mm:ss'
+    )
+  const age = moment().diff(birthday, 'years', false)
 
-const getClientById = id =>{}
+  const newData = { ...payload, birthday, age}
+
+  return await Clients.create(newData)
+}
+
+const getClientByName = name => {}
+
+const getClientById = id => {}
 
 const removeClient = id => {}
 
 const updateClientName = (id, name) => {}
 
 module.exports = {
-    createClient,
-    getClientByName,
-    getClientById,
-    removeClient,
-    updateClientName
+  createClient,
+  getClientByName,
+  getClientById,
+  removeClient,
+  updateClientName
 }
