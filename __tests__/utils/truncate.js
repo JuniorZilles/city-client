fa59def561ac61fd3e094c714352afaa3607b441
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-01 23:03:19 -0300

    feat: include ordered version to truncate tables

--- __tests__/utils/truncate.js
+++ __tests__/utils/truncate.js
@@ -1,10 +1,16 @@
 const { sequelize } = require('../../src/app/models')
 
-// limpa as tabelas do banco, percorrendo cada model e removendo os dados 
-module.exports = () => {
-  return Promise.all(
-    Object.keys(sequelize.models).map(key => {
-      return sequelize.models[key].destroy({ truncate: true, force: true })
-    })
-  )
+// limpa as tabelas do banco, percorrendo cada model e removendo os dados
+module.exports = {
+  truncate: () => {
+    return Promise.all(
+      Object.keys(sequelize.models).map(key => {
+        return sequelize.models[key].destroy({ truncate: true, force: true, restartIdentity: true  })
+      })
+    )
+  },
+  truncateOrdered: async () => {
+    await sequelize.models['Clients'].destroy({ truncate: true, force: true, restartIdentity: true  })
+    await sequelize.models['Cities'].destroy({ truncate: true, force: true, restartIdentity: true  })
+  }
 }
