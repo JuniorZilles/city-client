2fd78b5c6c9613a765d3a65fdf91fc089cabf60f
JuniorZilles <42867787+JuniorZilles@users.noreply.github.com>
2021-10-04 10:35:02 -0300

    feat: include swagger for documentation of the API

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,8 @@
 const router = require('express').Router();
+const swaggerUi = require('swagger-ui-express')
+const swaggerFile = require('./static/swagger_layout.json')
+
+router.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerFile))
 
 const cities = require("./app/controllers/CitiesController")
 router.use("/api/v1/cities",cities)
